7424281cc9a0a2aaa94117ee0091897880ece7f3
Dankcuddlybear <dankcuddlybear@proton.me>
2025-12-13 19:59:15 +0000

    Add ZestISO installer

--- zestiso-kde-gaming/profiledef.sh
+++ zestiso-kde-gaming/profiledef.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 iso_name="zestiso"
 iso_label="ZEST_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%y%m%d)"
-iso_publisher="ZestISO <https://github.com/dankcuddlybear/zestiso>"
+iso_publisher="ZestISO <https://github.com/SuperSonic65535/zestiso>"
 iso_application="ZestISO"
 iso_version="kde-gaming-$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
@@ -13,8 +13,9 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '22' '-b' '1M' '-not-reproducible')
+# Use all threads for mksquashfs if total is 4 or less, otherwise use half of total threads
+#AIROOTFS_THREADS="$([ $(nproc) -le 4 ] && nproc || expr $(nproc) / 2)"
+AIROOTFS_THREADS=16
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '22' '-b' '1M' '-not-reproducible' '-processors' $AIROOTFS_THREADS)
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
-file_permissions=(
-  ["/etc/calamares-custom/finalise-install.sh"]="0:0:755"
-)
+file_permissions=()
